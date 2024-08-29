cd834e2537451e00f379623bef01e45ccc215441
CharleyCosio <106895059+CharleyCosio@users.noreply.github.com>
2024-08-29 13:49:11 -0600

    Primer commit

---
+++ useFetch.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react"
+
+const localCache = {}
+
+export const useFetch = (url) => {
+
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: false,
+        error: null,
+    })
+
+    useEffect(() => {
+
+        getFetch()
+
+    }, [url])
+    //useEffect ejecutarse cada que cambia la url
+
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null,
+        })
+    }
+
+    const getFetch = async () => {
+
+        //Desde el cache
+        if (localCache[url]) {
+            setState({
+                data: localCache[url],
+                isLoading: false,
+                hasError: false,
+                error: null,
+            })
+            return;
+        }
+
+        setLoadingState()
+
+        const resp = await fetch(url)
+
+        //sleep
+        await new Promise(resolve => setTimeout(resolve, 1500))
+
+
+        if (!resp.ok) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                error: resp.statusText,
+            })
+            return;
+        }
+
+        const data = await resp.json();
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null,
+        })
+        // console.log(data)
+
+        //Manejo del cache
+        localCache[url] = data;
+
+    }
+
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError,
+    }
+}
---
+++ useTodo.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer.js";
+
+export const useTodo = () => {
+  
+    const initialState = [
+        // {
+        //     id: new Date().getTime(),
+        //     description: 'Recolectar la piedra del tiempo',
+        //     done: false
+        // },
+        ];
+        //Esta funcion es el tercer parametro del hook reducer y
+        // es el estado inicial del arreglo de objeto lo mismo
+        //que haciamos arriba en initialState pero en una funcion
+        //que toda los datos del localStorage se pude hacer de ambas formas
+        const init=()=>{
+            return JSON.parse(localStorage.getItem('todos')) || [];
+        }
+
+        const [todos, dispatch] = useReducer(todoReducer, initialState,init)
+
+        useEffect(() => {
+          localStorage.setItem('todos', JSON.stringify(todos))
+        }, [todos])
+        
+    
+        const handleNewTodo = (todo) => {
+            const action={
+                type: '[TODO] Add Todo',
+                payload: todo
+            }
+    
+            dispatch(action);
+        }
+        const handleDeleteTodo = (id) => {
+            const action={
+                type: '[TODO] Remove Todo',
+                payload: id
+            }
+    
+            dispatch(action);
+        }
+        const handleToggleTodo = (id) => {
+            dispatch({
+                type: '[TODO] Toggle Todo',
+                payload: id
+            });
+        }
+
+        const todosCount=todos.length;
+        const pendingTodosCount=todos.filter(todo=>!todo.done).length;
+    
+  
+    return {
+        todos,
+        todosCount,
+        pendingTodosCount,
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo
+    }
+}
